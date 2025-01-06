b1d1c07d758ca459fc88b4526b46b7c952cb723c
widyaahm <widyaherry10@gmail.com>
2025-01-06 20:42:07 +0700

    question

--- lib/controller/QuestionController.dart
+++ lib/controller/QuestionController.dart
@@ -1,1 +1,32 @@
+import 'package:get/get.dart';
 
+class QuestionController extends GetxController {
+  // For single selection (radio buttons)
+  final Rx<String> selectedSingleAnswer = ''.obs;
+  final RxList<String> selectedMultipleValues = <String>[].obs;
+
+  final RxMap singleAnswerObject = {}.obs;
+  final RxList<Map> multipleAnswerObject = <Map>[].obs;
+
+  bool isOptionSelected(String option) {
+    return selectedMultipleValues.contains(option);
+  }
+
+  void updateMultipleSelection(
+      int optionId, String optionText, bool isSelected) {
+    if (isSelected && !selectedMultipleValues.contains(optionText)) {
+      selectedMultipleValues.add(optionText);
+      multipleAnswerObject.add({'optionId': optionId, 'options': optionText});
+    } else if (!isSelected && selectedMultipleValues.contains(optionText)) {
+      selectedMultipleValues.remove(optionText);
+    }
+    update();
+  }
+
+  void clearSelections() {
+    selectedMultipleValues.clear();
+    update();
+  }
+
+  int get selectedCount => selectedMultipleValues.length;
+}
