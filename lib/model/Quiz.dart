bbeeda7187f36f7730b684da538d1039c83bb8b8
Fariz <farizrjanuar@gmail.com>
2025-01-06 22:36:18 +0700

    feat - Quzife model

--- lib/model/Quiz.dart
+++ lib/model/Quiz.dart
@@ -1,1 +1,22 @@
+import 'package:flutter/foundation.dart';
+import 'package:wave_education/model/Question.dart';
 
+class Quiz {
+  int quizID;
+  int? materiID;
+  int rewardPoints;
+  List<Question>? question = [];
+
+  Quiz(
+      {required this.quizID,
+      this.materiID,
+      required this.rewardPoints,
+      this.question});
+
+  factory Quiz.fromJson(Map<String, dynamic> json) {
+    return Quiz(
+      quizID: json['quizId'],
+      rewardPoints: json['rewardPoint'],
+    );
+  }
+}
